85664699b7f04a6a0afc51dd3613a014ecca516b
Bradley Bell <bradleyb@uw.edu>
2016-10-16 02:53:02 -0700

    account api fix

--- wheniwork_restclient/wheniwork/account.py
+++ wheniwork_restclient/wheniwork/account.py
@@ -3,42 +3,14 @@ from wheniwork_restclient.models import Account
 
 
 class Accounts(WhenIWork):
-    def get_account(self, account_id=""):
+    def get_account(self):
         """
-        Return primary account resource, or account for given wheniwork
-        account id.
+        Get Existing Account
 
-        http://dev.wheniwork.com/#listing-accounts
         http://dev.wheniwork.com/#get-existing-account
         """
-        url = "/2/account/%s" % account_id
+        url = "/2/account/"
         return self._account_from_json(self._get_resource(url)["account"])
-
-    def get_accounts(self):
-        """
-        Return list of accounts.
-
-        http://dev.wheniwork.com/#listing-accounts
-        """
-        url = "/2/account"
-
-        accounts = []
-        for entry in self._get_resource(url):
-            accounts.append(self._account_from_json(entry))
-
-        return accounts
-
-    def update_account(self, account):
-        """
-        Update the passed account. Returns the updated account.
-
-        http://dev.wheniwork.com/#create/update-account
-        """
-        url = "/2/account/%s" % account.account_id
-        body = {"account": {"name": account.name}}
-
-        data = self._put_resource(url, body)
-        return self._account_from_json(data)
 
     def _account_from_json(self, data):
         account = Account()
