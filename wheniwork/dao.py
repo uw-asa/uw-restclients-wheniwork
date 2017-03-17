3e957e939fa1ab4a003202c250f5895c2da04312
Bradley Bell <bradleyb@uw.edu>
2017-03-17 03:26:35 -0700

    use default host

--- wheniwork/dao.py
+++ wheniwork/dao.py
@@ -9,3 +9,7 @@ class WhenIWork_DAO(DAO):
 
     def service_mock_paths(self):
         return [abspath(join(dirname(__file__), "resources"))]
+
+    def get_default_service_setting(self, key):
+        if "HOST" == key:
+            return 'https://api.wheniwork.com'
